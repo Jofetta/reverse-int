3e2298f94ef7a71fdc027192aa96b7024f4a78b5
jofetta <jafarovatatiana@gmail.com>
2023-01-13 02:00:24 +0300

    feat:solve the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let newString = Math.abs(n).toString();
+    
+    let result = []; 
+    for (let i = newString.length; i > 0; i-- ){
+      
+    result = result + newString[i - 1];
+
+    }
+   
+    return result;
 }
